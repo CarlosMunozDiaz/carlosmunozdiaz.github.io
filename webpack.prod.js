86baf39c2eb08dacb50522efa2fd2d35fb809271
Carlos Munoz Diaz <cmunoz@elconfidencial.com>
2020-09-21 12:36:20 +0200

    Prueba enrutado con /docs

--- webpack.prod.js
+++ webpack.prod.js
@@ -9,7 +9,7 @@ module.exports = merge(common, {
 	mode: "production",
 	output: {
 		filename: "main.[contentHash].js",
-		path: path.resolve(__dirname, "root/")
+		path: path.resolve(__dirname, "docs/")
 	},
 	optimization: {
 		minimizer: [
