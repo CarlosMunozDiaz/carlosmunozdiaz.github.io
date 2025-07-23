23a15374ad22ebdaeeed3e8aea6eb309bd51712c
CarlosMunozDiaz <carlos10md@gmail.com>
2025-07-23 17:14:26 +0200

    No se abre una nueva página

---
+++ src/routes/bitacora/posts/test/+page.svelte
@@ -0,0 +1,7 @@
+<div class="content" view-transition-name="page">
+    <h1>Entrada de bitácora</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed euismod, urna eu tincidunt consectetur, nisi nisl aliquam enim, eget facilisis quam felis id mauris.</p>
+    <p>Phasellus euismod, sapien eu facilisis cursus, enim erat dictum urna, nec dictum velit enim at erat. Pellentesque habitant morbi tristique senectus et netus.</p>
+    <p>Curabitur ac leo nunc. Vestibulum et mauris vel ante finibus maximus nec ut leo. Integer consectetur luctus erat, nec dictum erat.</p>
+    <p>Morbi euismod, massa eget dictum dictum, enim erat dictum urna, nec dictum velit enim at erat. Etiam euismod, sapien eu facilisis cursus, enim erat dictum urna.</p>
+</div>
