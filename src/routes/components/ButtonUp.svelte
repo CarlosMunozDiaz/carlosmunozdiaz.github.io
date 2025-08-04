f1ec7967ffb468c32d09bea61047175305453e82
CarlosMunozDiaz <carlos10md@gmail.com>
2025-08-04 12:53:36 +0200

    Transición y cambio de estilo en botón

--- src/routes/components/ButtonUp.svelte
+++ src/routes/components/ButtonUp.svelte
@@ -1,6 +1,7 @@
 <script>
     import { onMount, onDestroy } from 'svelte';
     import { writable } from 'svelte/store';
+    import { fly } from 'svelte/transition';
 
     const show = writable(false);
 
@@ -29,13 +30,21 @@
         right: 2rem;
         bottom: 2rem;
         z-index: 1000;
+
+        background-color: #1e1f20;
+        border: none;
+        color: white;
+        cursor: pointer;
+        font-size: 18px;
+        line-height: 48px;
+        width: 48px;
     }
 </style>
 
 {#if $show}
     <div class="button-up">
-        <button on:click={() => scrollTo({ top: 0, behavior: 'smooth' })}>
-            Volver arriba
+        <button on:click={() => scrollTo({ top: 0, behavior: 'smooth' })} transition:fly={{ y: 100, duration: 2000 }}>
+            ☝️
         </button>
     </div>
 {/if}
